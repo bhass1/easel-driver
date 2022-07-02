acac2166c4e178d9e49eb0cd8a92f2e29afe4ad6
Bill Hass <billhass@umich.edu>
2022-07-02 13:34:49 +0000

    refactor: Remove command concatenation and fail early fix: bug where nvm not loaded due to no if statement

--- easel-driver.sh
+++ easel-driver.sh
@@ -1,36 +1,39 @@
-#!/bin/sh -x
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
 
 # Move previous out of way if exists
-if [ -e 'easel-driver' ]; then mv easel-driver easel-driver.bak.`date +%s`; fi &&
+if [ -e 'easel-driver' ]; then mv easel-driver easel-driver.bak.`date +%s`; fi
 
 # Create dir to work in
-mkdir -p easel-driver &&
-cd easel-driver &&
+mkdir -p easel-driver
+cd easel-driver
 
 # Download latest Easel Driver for Mac (which we'll extract necessary components from)
-curl -L https://easel.inventables.com/downloads | perl -ne 'print $1 if /href="([^"]+EaselDriver\S*\.pkg[^"]*)/' | xargs curl -o EaselDriver.pkg &&
+curl -L https://easel.inventables.com/downloads | perl -ne 'print $1 if /href="([^"]+EaselDriver\S*\.pkg[^"]*)/' | xargs curl -o EaselDriver.pkg
 
 # Install p7zip to unpack xar archive, avrdude for firmware upgrades, screen to run in background
-sudo apt-get install -y p7zip-full avrdude screen &&
+sudo apt-get install -y p7zip-full avrdude screen
 
 # Unpack Easel Driver
-7z x EaselDriver.pkg &&
+7z x EaselDriver.pkg
 
 # Unpack the primary Easel files
-cd IrisLib*.pkg &&
-zcat Payload | cpio -idv &&
+cd IrisLib*.pkg
+zcat Payload | cpio -idv
 
 # Grab the necessary files
-cp -r lib iris.js package.json ssl avrdude/etc/avrdude.conf ../ &&
-cd .. &&
+cp -r lib iris.js package.json ssl avrdude/etc/avrdude.conf ../
+cd ..
 
 # Move avrdude.conf into lib/etc as that's where the easel driver will look
-mkdir lib/etc &&
-mv avrdude.conf lib/etc &&
-ln -s lib/etc etc &&
+mkdir lib/etc
+mv avrdude.conf lib/etc
+ln -s lib/etc etc
 
 # Modify the firmware uploader to support Linux
-perl -pi -e 'if (/var PLATFORMS/) { $x = chr(39); print; $_ = "\t${x}Linux${x}: {\n\t\troot: ${x}/usr/bin/avrdude${x},\n\t\texecutable: ${x}/usr/bin/avrdude${x},\n\t\tconfig: path.join(__dirname, ${x}etc/avrdude.conf${x})\n\t},\n"; }' lib/firmware_uploader.js &&
+perl -pi -e 'if (/var PLATFORMS/) { $x = chr(39); print; $_ = "\t${x}Linux${x}: {\n\t\troot: ${x}/usr/bin/avrdude${x},\n\t\texecutable: ${x}/usr/bin/avrdude${x},\n\t\tconfig: path.join(__dirname, ${x}etc/avrdude.conf${x})\n\t},\n"; }' lib/firmware_uploader.js
 
 # Modify the serial port code to support CH340/CH341-based serial devices by spoofing an FTDI chip
 perl -pi -e 'if (/callback\(ports\)/) { print << "EOF"
@@ -41,12 +44,14 @@ perl -pi -e 'if (/callback\(ports\)/) { print << "EOF"
             this[i].vendorId = "0403";
         }, ports);
 EOF
-}' lib/serial_port_controller.js &&
+}' lib/serial_port_controller.js
 
 # Install nodejs using nvm
 # The installation script will clone the nvm repository from Github to the ~/.nvm directory 
 # and add the nvm path to your Bash profile.
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash - &&
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash -
+
+NVM_DIR=
 
 # Rerun Profile script to start NVM
 if [ ! -e "$NVM_DIR" ]; then
@@ -57,45 +62,46 @@ if [ ! -e "$NVM_DIR" ]; then
                         export NVM_DIR="$HOME/.config/nvm"
                 else
                         echo "Can't find NVM directory!"
+                        exit 1
                 fi
         fi
-fi &&
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && # This loads nvm bash_completion
-. ~/.bashrc &&
+fi
+. "$NVM_DIR/nvm.sh"    # This loads nvm
+. "$NVM_DIR/bash_completion"    # This loads nvm bash_completion
+. ~/.bashrc
 
 # Ensure screen also respects the bashrc
-echo 'shell -$SHELL' >> ~/.screenrc &&
+echo 'shell -$SHELL' >> ~/.screenrc
 
 # install nodejs v12
-nvm install v12.19.0 &&
-nvm use v12.19.0 &&
+nvm install v12.19.0
+nvm use v12.19.0
 
 # Install the necessary node modules
-npm install &&
-echo "\n\n\n" &&
+npm install
+echo "\n\n\n"
 
 # Create a startup script
-#echo '. ~/.bashrc ; . ~/.nvm/nvm.sh ; nvm use' \'v12.19.0\' '; cd ~/easel-driver ; node iris.js' > run.sh &&
-#chmod 755 run.sh &&
+#echo '. ~/.bashrc ; . ~/.nvm/nvm.sh ; nvm use' \'v12.19.0\' '; cd ~/easel-driver ; node iris.js' > run.sh
+#chmod 755 run.sh
 
 # Allow installing on reboot
 check_init() { 
-        # Per sd_booted(8), the way to check if systemd is running is to check if this directory exists.
-        if [ -d /run/systemd/system/ ]; then 
-                # set if systemd is in use on this system.
-                SYSD=1
-        else
-                SYSD=0
-        fi
+    # Per sd_booted(8), the way to check if systemd is running is to check if this directory exists.
+    if [ -d /run/systemd/system/ ]; then 
+        # set if systemd is in use on this system.
+        SYSD=1
+    else
+        SYSD=0
+    fi
 }
 
 # Create simple start script and save in easel driver folder
 create_start_script() { 
-        driverdir=$(pwd)
-        touch ${driverdir}/run.sh
-        chmod +x ${driverdir}/run.sh
-        cat <<EOF > run.sh
+    driverdir=$(pwd)
+    touch ${driverdir}/run.sh
+    chmod +x ${driverdir}/run.sh
+    cat <<EOF > run.sh
 #!/bin/bash
 . ~/.bashrc
 . ~/.nvm/nvm.sh
@@ -109,7 +115,7 @@ EOF
 
 # Installs system specific service
 install_service() {
-        if [ "${SYSD}" = "1" ]; then
+    if [ "${SYSD}" = "1" ]; then
         touch ${driverdir}/EaselDriver.service
         cat <<EOF > ${driverdir}/EaselDriver.service
 [Unit]
@@ -127,7 +133,7 @@ EOF
         sudo mv ${driverdir}/EaselDriver.service /etc/systemd/system/
         sudo systemctl daemon-reload
         sudo systemctl enable EaselDriver.service
-        else # if init.d create start/stop script enable with chkconfig
+    else # if init.d create start/stop script enable with chkconfig
         touch ${driverdir}/EaselDriver
         cat <<EOF > ${driverdir}/EaselDriver
 #!/bin/bash
@@ -162,7 +168,7 @@ EOF
         sudo mv ${driverdir}/EaselDriver /etc/init.d/
         sudo chkconfig --add EaselDriver
         sudo chkconfig --level 2345 EaselDriver on
-fi
+    fi
 }
 
 while true; do
@@ -178,15 +184,15 @@ while true; do
     [Nn]* ) break;;
     * ) echo "Please answer yes/no";;
   esac
-done &&
+done
 
 # Profit (run driver in the background)
-screen -L -dmS easel node iris.js &&
+screen -L -dmS easel node iris.js
 
 # Output the screen log so we can see if it was successful
-sleep 3 &&
-echo "\n\n\n" &&
-tail screenlog.0 &&
+sleep 3
+echo "\n\n\n"
+tail screenlog.0
 
 # Run `screen -r easel` to access the driver, and Ctrl+A (Cmd+A on macOS) followed by 'd' to detach)
 echo '\n\nDone! Easel driver running in background. Run `screen -r` to bring it to foreground.'
